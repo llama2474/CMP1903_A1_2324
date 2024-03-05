486e18af486020bfc1c90494b49a38885fa341d8
llama2474 <wiktorledzion@gmail.com>
2024-03-05 16:53:00 +0000

    Updated Game

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -16,12 +16,12 @@ namespace CMP1903_A1_2324
         }
 
         private int _diceVal = 0;
-
-        Random random = new Random(); //Creates a new Random object
+        
         
         public int Roll()
         {
             Thread.Sleep(1); //Pauses To Randomize the dice rolls
+            Random random = new Random(); //Creates a new Random object
             _diceVal = random.Next(1,7); //Generates a random number between 1 and6
             Console.WriteLine($"You rolled a {_diceVal}");
             return _diceVal;
